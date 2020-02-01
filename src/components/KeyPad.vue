9526407f09e92f139ec8171d3073462d547c1e24
Kyle Geske <stungeye@gmail.com>
2020-02-01 14:39:09 -0600

    After installing and configuring vetur, eslint and prettier vs code plugins.

---
+++ src/components/KeyPad.vue
@@ -0,0 +1,190 @@
+<template>
+  <div :class="'keypad-dialog ' + animation">
+    <div class="keypad-container">
+      <template v-for="n in 12">
+        <div :key="n" class="keypad-flex keypad-class">
+          <div v-if="n == 10 && onReset" class="keypad" @click="onReset">
+            <div class="keypad-center">
+              <strong class="keypad-delete">❌</strong>
+            </div>
+          </div>
+          <div
+            v-if="n != 10 && n != 12"
+            class="keypad"
+            :ripple="true"
+            @click="onInput(n)"
+          >
+            <div v-if="n < 10" class="keypad-center">{{ n }}</div>
+            <div v-if="n == 11" class="keypad-center">0</div>
+          </div>
+          <div v-if="n == 12 && onSubmit" class="keypad" @click="onSubmit(n)">
+            <div v-if="n == 12" class="keypad-center">
+              <strong class="keypad-delete">✔️</strong>
+            </div>
+          </div>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "KeyPad",
+  props: {
+    keypadClass: { type: String, default: "keypad-class", required: false },
+    show: { type: Boolean, default: true, required: false },
+    close: { type: String, default: "Close", required: false },
+    onInput: { type: Function, required: true },
+    onSubmit: { type: Function, required: false, default: function() {} },
+    onDelete: { type: Function, required: false, default: function() {} },
+    onReset: { type: Function, required: false, default: function() {} }
+  },
+  data: () => ({
+    n: 0,
+    animation: "keypad-hide"
+  }),
+  watch: {
+    show() {
+      this.animation === "slideInUp"
+        ? (this.animation = "slideOutDown")
+        : (this.animation = "slideInUp");
+    }
+  },
+  mounted() {
+    this.show ? (this.animation = "slideInUp") : (this.animation = "hide");
+  },
+  methods: {
+    closeMe() {
+      this.animation = "slideOutDown";
+    }
+  }
+};
+</script>
+
+<style>
+.keypad-hide {
+  visibility: hidden;
+}
+
+.keypad-class {
+  color: #888;
+  background: #fafafa;
+  border: 0.004rem solid #eaeaea;
+}
+
+.keypad-dialog {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+}
+
+.keypad-container {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-flex: 1;
+  -ms-flex: 1 1 auto;
+  flex: 1 1 auto;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  min-width: 0;
+  flex-direction: row;
+}
+
+.keypad-value {
+  width: 100%;
+  text-align: right;
+  padding: 0.5rem;
+}
+
+.keypad-flex {
+  flex-basis: 33%;
+  -webkit-box-flex: 0;
+  -ms-flex-positive: 0;
+  flex-grow: 0;
+  max-width: 33%;
+  min-height: 4rem;
+}
+
+.keypad {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  vertical-align: center;
+  margin: 0 auto;
+}
+
+.keypad-center {
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 1.3rem;
+}
+
+.keypad-delete {
+  font-size: 1.5rem;
+}
+.slideInUp {
+  -webkit-animation-name: slideInUp;
+  animation-name: slideInUp;
+  -webkit-animation-duration: 1s;
+  animation-duration: 1s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+}
+@-webkit-keyframes slideInUp {
+  0% {
+    -webkit-transform: translateY(100%);
+    transform: translateY(100%);
+    visibility: visible;
+  }
+  100% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+}
+@keyframes slideInUp {
+  0% {
+    -webkit-transform: translateY(100%);
+    transform: translateY(100%);
+    visibility: visible;
+  }
+  100% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+}
+
+.slideOutDown {
+  -webkit-animation-name: slideOutDown;
+  animation-name: slideOutDown;
+  -webkit-animation-duration: 1s;
+  animation-duration: 1s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+}
+@-webkit-keyframes slideOutDown {
+  0% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+  100% {
+    visibility: hidden;
+    -webkit-transform: translateY(100%);
+    transform: translateY(100%);
+  }
+}
+@keyframes slideOutDown {
+  0% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+  100% {
+    visibility: hidden;
+    -webkit-transform: translateY(100%);
+    transform: translateY(100%);
+  }
+}
+</style>
---
+++ src/components/QuizGame.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="game">
+    <div class="question">
+      <div class="operand">
+        5
+      </div>
+      <div class="operator">
+        +
+      </div>
+      <div class="operand">
+        4
+      </div>
+    </div>
+    <div>Answer: {{ number }}</div>
+    <keypad
+      :on-input="onInput"
+      :on-submit="onSubmit"
+      :on-reset="onReset"
+      :show="showKeypad"
+    />
+  </div>
+</template>
+
+<script>
+import keypad from "@/components/KeyPad.vue";
+
+export default {
+  name: "QuizGame",
+  components: {
+    keypad
+  },
+  data: () => ({
+    number: "",
+    maxLength: 6,
+    showKeypad: true
+  }),
+  methods: {
+    onInput(key) {
+      this.number = (this.number + key).slice(0, this.maxLength);
+    },
+    onSubmit() {
+      alert("Submitted: " + this.number);
+    },
+    onReset() {
+      this.number = "";
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.game {
+  font-size: 56px;
+}
+.question {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(3, auto);
+}
+</style>
