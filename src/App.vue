b2ca0c4810074d20d7050d1f81bcf8c5d5cf5c81
Kyle Geske <stungeye@gmail.com>
2020-02-29 07:26:23 -0600

    Added the Vue router for multipage app.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
   <div id="app">
-    <QuizGame />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
-
-<script>
-import QuizGame from "./components/QuizGame";
-
-export default {
-  name: "App",
-  components: {
-    QuizGame
-  }
-};
-</script>
 
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
-  margin-top: 1em;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <QuizGame />
+  </div>
+</template>
+
+<script>
+import QuizGame from "../components/QuizGame";
+
+export default {
+  name: "App",
+  components: {
+    QuizGame
+  }
+};
+</script>
